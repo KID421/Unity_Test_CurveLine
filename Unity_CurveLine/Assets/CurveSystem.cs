6fae65c2bf5e3814a1ee0cb19516c2d2e9a1a82f
KID <50666207+KID421@users.noreply.github.com>
2020-02-25 20:47:55 +0800

    完成網頁版

---
+++ Unity_CurveLine/Assets/CurveSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CurveSystem : MonoBehaviour
+{
+    public GameObject ballLine;
+    public GameObject ball;
+
+    public Transform weapon;
+
+    private void Update()
+    {
+        RotateWeapon();
+        CreateBall();
+    }
+
+    private void RotateWeapon()
+    {
+        //weapon.Rotate(Time.deltaTime, 0, 0);
+    }
+
+    private void CreateBall()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            GameObject temp = Instantiate(ballLine, weapon.position, weapon.rotation);
+            temp.GetComponent<Rigidbody>().AddForce(-weapon.forward * 500);
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject temp = Instantiate(ball, weapon.position, weapon.rotation);
+            temp.GetComponent<Rigidbody>().AddForce(-weapon.forward * 500);
+        }
+    }
+}
